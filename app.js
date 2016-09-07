e40ed8bedef685c35853ac16ae33cd33ba384bf9
FedeLonghi <fedee.29@hotmail.com>
2016-09-07 12:33:44 -0300

    arreglo problema de path de resources

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require ('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static(__dirname + '/public'));
+app.use('/node_modules', express.static(__dirname + "/node_modules"));
+app.use('/images', express.static(__dirname + "/images"));
+app.use('/', express.static(__dirname));
+
+console.log(__dirname);
+
+app.get('/jobify', function(request, response){
+	response.sendFile(__dirname + "/" + "Jobify.html");
+});
 
 app.get('/index.html', function(request, response){
 	response.sendFile(__dirname + "/" + "index.html");
@@ -11,6 +23,15 @@ app.get("/process_get", function(request, response){
 	res = {
 		first_name: request.query.first_name,
 		last_name: request.query.last_name,
+	};
+	console.log(res);
+	response.end(JSON.stringify(res));
+});
+
+app.post("/process_post", urlencodedParser, function(request, response){	
+	res = {
+	first_name: request.body.first_name,
+	last_name: request.body.last_name
 	};
 	console.log(res);
 	response.end(JSON.stringify(res));
