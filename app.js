b6c94a8660da93b500bb1c2333f700aa25733e0d
FedeLonghi <fedee.29@hotmail.com>
2016-09-09 15:27:01 -0300

    cambios en configuracion

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var pg = require('pg');
-
 var app = express();
-var bodyParser = require ('body-parser');
 
+var bodyParser = require ('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+// setting views
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//esto hay que cambiar ->
 app.use('/node_modules', express.static(__dirname + "/node_modules"));
 app.use('/images', express.static(__dirname + "/images"));
 app.use('/', express.static(__dirname));
-
-console.log(__dirname);
+//<-
 
 app.get('/jobify', function(request, response){
 	response.sendFile(__dirname + "/" + "Jobify.html");
@@ -44,7 +48,7 @@ app.post("/process_post", urlencodedParser, function(request, response){
 app.get("/job_positions", function(request, response){
 	pos = positions;
 	response.end(JSON.stringify(pos));
-});
+});		
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
