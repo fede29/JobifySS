d2b05af54fa99ab2fa64ca2013ba1b54b3570a4d
FedeLonghi <fedee.29@hotmail.com>
2016-09-20 18:04:35 -0300

    devuelvo lista completa desde la base de datos

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var pg = require('pg');
-var app = express();
 var massive  = require('massive');
-//var connectionString = process.env.DATABASE_URL;
-var connectionString = process.env.DATABASE_URL || "postgres://fedelonghi:admin123@localhost/localdb";
+var bodyParser = require ('body-parser');
+var serveStatic = require('serve-static');
+var multer = require('multer');
+
+var config = require('./config.json');
+
+var connectionString = process.env.DATABASE_URL || "postgres://"+config.postgres.user+":"+config.postgres.password+"@"+config.postgres.host+"/"+config.postgres.db;
 
 var massiveInstance = massive.connectSync({connectionString: connectionString});
 
+var app = express();
+
 app.set('db', massiveInstance);
-var db = app.get('db');
 
-var new_jpos = {
-	name : 'test_name',
-	description : 'test_description',
-	category: 'test_category'
-};
-
-var bodyParser = require ('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('port', (process.env.PORT || 5000));
@@ -30,10 +28,41 @@ app.set('view engine', 'ejs');
 //esto hay que cambiar ->
 app.use('/node_modules', express.static(__dirname + "/node_modules"));
 app.use('/images', express.static(__dirname + "/images"));
-//app.use('/', express.static(__dirname));
 //<-
 
+var handleError = function(res){
+	return function(err){
+		console.log(err);
+		res.send(500,{error: err.message});
+	}
+}
 
+var db = app.get('db');
+/*
+db.job_positions.find({name : "developer"}, function(err, res){
+	console.log(res);
+});
+
+var new_jpos = {
+	name : 'test_name',
+	description : 'test_description',
+	category: 'test_category'
+};
+
+db.job_positions.save(new_jpos, function(err, result){
+	console.log(result);
+});
+
+db.job_positions.find({name:"test_name"}, function(err, res){
+	console.log(res);
+});
+
+db.query("SELECT * FROM job_positions", function(err, data){
+	//res.status(200).send(data);
+	console.log(data);
+});
+
+*/
 //solo de prueba ->
 app.get('/test', function(request, response){
 	response.sendFile(__dirname + "/" + "index.html");
@@ -59,9 +88,23 @@ app.post("/process_post", urlencodedParser, function(request, response){
 //<-
 
 app.get("/job_positions", function(request, response){
-	pos = positions;
-	response.end(JSON.stringify(pos));
+	db.query("SELECT * FROM job_positions", function (err, data){
+		response.status(200).send(data);
+	});
 });
+
+app.get("/categories", function(request, response){
+	db.query("SELECT * FROM categories", function (err, data){
+		response.status(200).send(data);
+	});
+});
+
+app.get("/skills", function(request, response){
+	db.query("SELECT * FROM skills", function (err, data){
+		response.status(200).send(data);
+	});
+});
+	
 
 
 //inicio de app
@@ -69,24 +112,7 @@ app.get('/', function(request, response){
 	response.render('pages/jobify');
 });
 
-app.get('/nv', function(request, response){
-	response.render('pages/nv');
-});
-
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
-
-
-//posiciones hardcodeadas
-var positions = [{
-	"name": "developer",
-	"description": "a software developer",
-	"category": "software"
-	},{
-	"name": "project manager",
-	"description": "a project manager",
-	"category": "management"
-	}
-];
 
