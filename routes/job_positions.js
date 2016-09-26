b8c709bcb01aa72ac1ab72e75eab27f30c25f73d
FedeLonghi <fedee.29@hotmail.com>
2016-09-25 22:45:57 -0300

    agrego metadata

--- routes/job_positions.js
+++ routes/job_positions.js
@@ -3,27 +3,28 @@ var router = express.Router();
 
 router.get('/job_positions',function(req,res,next){
 	req.db.query("SELECT * FROM job_positions", function (err, job_positions){
-		var meta_data={
+		var metadata={
 			version:0.1,
 			count:1
 		};
 		var data = {
 			job_positions,
-			meta_data
+			metadata
 		};	
 		res.status(200).send(data);
 	});
 });
 
 router.get('/job_positions/categories/:category',function(req,res,next){
-	//res.status(200).send(req.params.category);
-	console.log("1");
-	var query = "SELECT * FROM job_positions where category=";
-	query+=req.params.category;
-	console.log(query)
-	req.db.query(query, function(err, data){
-		console.log("2");
-		console.log(data)
+	req.db.job_positions.find({category:req.params.category}, function(err, job_positions){
+		var metadata={
+			version:0.1,
+			count:1
+		};
+		var data = {
+			job_positions,
+			metadata
+		};
 		res.status(200).send(data);
 	});
 });
--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/skills", function(request, response){
-	request.db.query("SELECT * FROM skills", function (err, data){
-		response.status(200).send(data);
+router.get("/skills", function(req, res, next){
+	req.db.query("SELECT * FROM skills", function (err, skills){
+		var metadata={
+			version:0.1,
+			count:1
+		};
+		var data={
+			skills,
+			metadata
+		};
+		res.status(200).send(data);
 	});
 });
+
+router.get("/skills/categories/:category", function(req, res, next){
+	req.db.skills.find({category:req.params.category}, function(err, skills){
+		var metadata={
+			version:0.1,
+			count:1
+		};
+		var data = {
+			skills,
+			metadata
+		};
+		res.status(200).send(data);
+	});
+});
+	
+	
 
 module.exports = router;
