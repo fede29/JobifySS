a024d46cf604728516bc1c68af12daea0e0ee40d
FedeLonghi <fedee.29@hotmail.com>
2016-10-03 20:06:14 -0300

    agrego todos los metodos de job_positions

--- routes/job_positions.js
+++ routes/job_positions.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+//listar todo
 router.get('/job_positions',function(req,res,next){
 	req.db.query("SELECT * FROM job_positions", function (err, job_positions){
 		var metadata={
@@ -13,8 +14,10 @@ router.get('/job_positions',function(req,res,next){
 		};	
 		res.status(200).send(data);
 	});
+	//Error 500, Unexpected error.
 });
 
+//listar por categoria
 router.get('/job_positions/categories/:category',function(req,res,next){
 	req.db.job_positions.find({category:req.params.category}, function(err, job_positions){
 		var metadata={
@@ -27,6 +30,37 @@ router.get('/job_positions/categories/:category',function(req,res,next){
 		};
 		res.status(200).send(data);
 	});
+	//Error 404, Categoria Inexistente
+	//Error 500, Unexpected Error
+});
+
+//Alta de puesto
+router.post('/job_positions/categories/:category', function(req, res, next){
+	req.db.job_positions.insert({name:req.body.name,description:req.body.description,category:req.params.category},function (err,data){
+		res.status(201).send(data);
+	});
+	//Error 400 Incumplimiento de Precondiciones
+	//Error 404 Categoria Inexistente
+	//Error 500 Unexpected Error
+});
+
+
+//Baja de Puesto
+router.delete('/job_positions/categories/:category/:name', function(req, res, next){
+	req.db.job_positions.destroy({name:req.params.name, category:req.params.category},function(err, data){
+		res.status(204).send(data);
+	});
+	//Error 404 No existe el recurso solicitado
+	//Error 500 Unexpected Error
+});
+
+//Modificacion de puesto
+router.put('/job_positions/categories/:category/:name', function(req, res, next){
+	req.db.job_positions.update({name:req.params.name, category:req.params.category, description:req.body.description},function(err, data){
+		res.status(200).send(data);
+	});
+	//Error 404 No existe el recurso solicitado
+	//Error 500 Unexpected Error
 });
 
 module.exports = router;
