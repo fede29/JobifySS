d8f7af49c08590dbb0f66f985b3f0a2e48a5a8ee
FedeLonghi <fedee.29@hotmail.com>
2016-10-03 20:39:46 -0300

    Completo todos los metodos

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+//Listar todo
 router.get("/categories", function(request, response){
 	request.db.query("SELECT * FROM categories", function (err, data){
 		response.status(200).send(data);
 	});
+});
+
+//Alta de puesto
+router.post('/categories', function(req, res, next){
+	req.db.categories.insert({name:req.body.name,description:req.body.description},function (err,data){
+		res.status(201).send(data);
+	});
+	//Error 400 Incumplimiento de Precondiciones
+	//Error 404 Categoria Inexistente
+	//Error 500 Unexpected Error
+});
+
+
+//Baja de Puesto
+router.delete('/categories/:name', function(req, res, next){
+	req.db.job_positions.destroy({name:req.params.name},function(err, data){
+		res.status(204).send(data);
+	});
+	//Error 404 No existe el recurso solicitado
+	//Error 500 Unexpected Error
+});
+
+//Modificacion de puesto
+router.put('/categories/:name', function(req, res, next){
+	req.db.job_positions.update({name:req.params.name, description:req.body.description},function(err, data){
+		res.status(200).send(data);
+	});
+	//Error 404 No existe el recurso solicitado
+	//Error 500 Unexpected Error
 });
 
 module.exports = router;
--- routes/skills.js
+++ routes/skills.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+//Listar todo
 router.get("/skills", function(req, res, next){
 	req.db.query("SELECT * FROM skills", function (err, skills){
 		var metadata={
@@ -15,6 +16,7 @@ router.get("/skills", function(req, res, next){
 	});
 });
 
+//Listar por categoria
 router.get("/skills/categories/:category", function(req, res, next){
 	req.db.skills.find({category:req.params.category}, function(err, skills){
 		var metadata={
@@ -29,6 +31,33 @@ router.get("/skills/categories/:category", function(req, res, next){
 	});
 });
 	
-	
+//Alta de puesto
+router.post('/skills/categories/:category', function(req, res, next){
+	req.db.skills.insert({name:req.body.name,description:req.body.description,category:req.params.category},function (err,data){
+		res.status(201).send(data);
+	});
+	//Error 400 Incumplimiento de Precondiciones
+	//Error 404 Categoria Inexistente
+	//Error 500 Unexpected Error
+});
+
+
+//Baja de Puesto
+router.delete('/skills/categories/:category/:name', function(req, res, next){
+	req.db.skills.destroy({name:req.params.name, category:req.params.category},function(err, data){
+		res.status(204).send(data);
+	});
+	//Error 404 No existe el recurso solicitado
+	//Error 500 Unexpected Error
+});
+
+//Modificacion de puesto
+router.put('/skills/categories/:category/:name', function(req, res, next){
+	req.db.skills.update({name:req.params.name, category:req.params.category, description:req.body.description},function(err, data){
+		res.status(200).send(data);
+	});
+	//Error 404 No existe el recurso solicitado
+	//Error 500 Unexpected Error
+});
 
 module.exports = router;
